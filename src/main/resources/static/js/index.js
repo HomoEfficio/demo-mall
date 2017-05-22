77b78937b34e6aa51d5a44b73d69d83a4c96af3c
Oh Myung Woon <omwomw@sk.com>
2017-05-22 09:57:18 +0900

    Signin, Signup 화면

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,4 +1,41 @@
 /**
  * Created by omwomw@sk.com on 2017. 5. 22..
  */
-console.log('Local JavaScript is OK');
+new Vue({
+    el: '#app',
+    data: {
+        userName: '',
+        // form: document.getElementById('sign-form'),
+        form: '',
+    },
+    methods: {
+        onSignIn(endPoint) {
+            console.log('onSignIn');
+            // GET /sign-in/{memberName}
+            this.form.method = 'GET';
+            this.form.action = endPoint + '/' + this.userName;
+            this.form.submit();
+
+            // axios.get(endPoint + '/' + this.userName)
+            //     .then(res => {
+            //         console.log(res);
+            //         document.location.href = './main.html'
+            //     })
+            //     .catch(err => {
+            //         console.log(err);
+            //     });
+        },
+        onSignUp(endPoint) {
+            console.log('onSignUp');
+            // POST /members/{memberName}
+            axios.post(endPoint + '/' + this.userName)
+                .then(res => {
+                    console.log(res);
+                    this.onSignIn('/sign-in');
+                })
+                .catch(err => {
+                    console.log(err);
+                });
+        },
+    }
+});
--- src/main/resources/templates/index.ftl
+++ src/main/resources/templates/index.ftl
@@ -6,8 +6,22 @@
     <link rel="stylesheet" href="https://unpkg.com/purecss@0.6.2/build/pure-min.css" integrity="sha384-UQiGfs9ICog+LwheBSRCt1o5cbyKIHbwjWscjemyBMT9YCUMZffs6UqUTd0hObXD" crossorigin="anonymous">
 </head>
 <body>
-<h1>Hi RecoPick</h1>
-<div id="app"></div>
+
+<div id="app">
+    <div class="header">
+        <h1>RecoPick Demo Mall</h1>
+    </div>
+    <div class="main">
+        <div class="sign">
+            <form id="sign-form" class="sign">
+                <input v-model="userName" placeholder="UserName을 입력하세요.">
+                <button class="btn" v-on:submit.prevent="onSignIn('/sign-in')">Sign In</button>
+                <button class="btn" v-on:submit.prevent="onSignUp('/api/members')">Sign Up</button>
+            </form>
+        </div>
+    </div>
+</div>
+
 <script src="https://unpkg.com/vue"></script>
 <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
 <script src="/js/index.js"></script>
