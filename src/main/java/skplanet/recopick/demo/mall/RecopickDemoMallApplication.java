9efcf9689cba64461870841dc23ea6810c01d92a
Oh Myung Woon <omwomw@sk.com>
2017-05-22 09:38:38 +0900

    Initial Commit

---
+++ src/main/java/skplanet/recopick/demo/mall/RecopickDemoMallApplication.java
@@ -0,0 +1,12 @@
+package skplanet.recopick.demo.mall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecopickDemoMallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecopickDemoMallApplication.class, args);
+	}
+}
