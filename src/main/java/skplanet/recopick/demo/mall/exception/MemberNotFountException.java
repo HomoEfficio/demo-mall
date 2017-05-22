151cf6571980f476a7c70a964d8afed5d38cc255
Oh Myung Woon <omwomw@sk.com>
2017-05-22 10:12:21 +0900

    MemberNotFoundExeption 추가

---
+++ src/main/java/skplanet/recopick/demo/mall/exception/MemberNotFountException.java
@@ -0,0 +1,8 @@
+package skplanet.recopick.demo.mall.exception;
+
+/**
+ * @author homo.efficio@gmail.com
+ *         created on 2017-05-22
+ */
+public class MemberNotFountException extends RuntimeException {
+}
