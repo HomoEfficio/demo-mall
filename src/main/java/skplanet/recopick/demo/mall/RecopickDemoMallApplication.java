d3ec88e276ea5e2abbb7e5563116f9f39e804b62
Oh Myung Woon <omwomw@sk.com>
2017-05-22 10:37:49 +0900

    Jpa Auditing 적용

--- src/main/java/skplanet/recopick/demo/mall/RecopickDemoMallApplication.java
+++ src/main/java/skplanet/recopick/demo/mall/RecopickDemoMallApplication.java
@@ -2,8 +2,16 @@ package skplanet.recopick.demo.mall;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
+@EntityScan(
+		basePackageClasses = {Jsr310JpaConverters.class},
+		basePackages = {"skplanet.recopick.demo.mall"}
+)
 public class RecopickDemoMallApplication {
 
 	public static void main(String[] args) {
