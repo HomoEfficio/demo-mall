55afe1a3937ea4f376876a9f29ac69ec3c52c688
Oh Myung Woon <omwomw@sk.com>
2017-05-22 10:44:15 +0900

    signup을 json 반환하도록 수정

--- src/main/java/skplanet/recopick/demo/mall/controller/ViewController.java
+++ src/main/java/skplanet/recopick/demo/mall/controller/ViewController.java
@@ -1,18 +1,17 @@
 package skplanet.recopick.demo.mall.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import skplanet.recopick.demo.mall.domain.Member;
-import skplanet.recopick.demo.mall.exception.MemberNotFountException;
 import skplanet.recopick.demo.mall.repository.MemberRepository;
 
 import java.util.Objects;
-import java.util.Optional;
 
 /**
  * @author homo.efficio@gmail.com
@@ -21,8 +20,15 @@ import java.util.Optional;
 @Controller
 public class ViewController {
 
+    private MemberRepository memberRepository;
+    private ObjectMapper objectMapper;
+
     @Autowired
-    MemberRepository memberRepository;
+    public ViewController(MemberRepository memberRepository,
+                          ObjectMapper objectMapper) {
+        this.memberRepository = memberRepository;
+        this.objectMapper = objectMapper;
+    }
 
     @GetMapping("/")
     public String index() {
@@ -30,10 +36,10 @@ public class ViewController {
     }
 
     @PostMapping("/members/{userName}")
-    public String signUp(@PathVariable("userName") String userName, Model model) {
+    @ResponseBody
+    public ResponseEntity<Member> signUp(@PathVariable("userName") String userName) {
         Member member = memberRepository.save(new Member(userName));
         Objects.requireNonNull(member, "Member is null");
-        model.addAttribute("member", member);
-        return "main";
+        return ResponseEntity.ok(member);
     }
 }
