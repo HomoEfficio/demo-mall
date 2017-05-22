468fa1d3902b88621d77538f530edf1218786a24
Oh Myung Woon <omwomw@sk.com>
2017-05-22 10:12:44 +0900

    SignUp 컨트롤러 핸들러 추가

--- src/main/java/skplanet/recopick/demo/mall/controller/ViewController.java
+++ src/main/java/skplanet/recopick/demo/mall/controller/ViewController.java
@@ -1,7 +1,17 @@
 package skplanet.recopick.demo.mall.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import skplanet.recopick.demo.mall.domain.Member;
+import skplanet.recopick.demo.mall.exception.MemberNotFountException;
+import skplanet.recopick.demo.mall.repository.MemberRepository;
+
+import java.util.Optional;
 
 /**
  * @author homo.efficio@gmail.com
@@ -10,8 +20,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class ViewController {
 
-    @RequestMapping("/")
+    @Autowired
+    MemberRepository memberRepository;
+
+    @GetMapping("/")
     public String index() {
         return "index";
+    }
+
+    @PostMapping("/members/{userName}")
+    public String signUp(@PathVariable("userName") String userName, Model model) {
+        Optional<Member> optMember = memberRepository.save(new Member(userName));
+        model.addAttribute("member", optMember.orElseThrow(() -> new MemberNotFountException()));
+        return "main";
     }
 }
